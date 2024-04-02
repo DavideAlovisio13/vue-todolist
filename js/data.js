62f80c2d5f5b5db2df1d71fb4cb9f8f46cc53e59
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-02 15:24:29 +0200

    (html) create html model

--- js/data.js
+++ js/data.js
@@ -1,31 +1,32 @@
-const toDoList = [
+
+export const toDoList = [
     {
         id: 1,
-        test: 'Fare i compiti',
+        text: 'Fare i compiti',
         done: false
     },
 
     {
         id: 2,
-        test: 'Fare i compiti',
+        text: 'Fare i compiti',
         done: false
     },
 
     {
         id: 3,
-        test: 'Fare i compiti',
+        text: 'Fare i compiti',
         done: false
     },
 
     {
         id: 4,
-        test: 'Fare i compiti',
+        text: 'Fare i compiti',
         done: false
     },
 
     {
         id: 5,
-        test: 'Fare i compiti',
+        text: 'Fare i compiti',
         done: false
     }
 ]
--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 import {toDoList} from './data.js'
-
+// destrutturazione del metodo createApp, mettendola nella variabile vue
 const {createApp} = Vue;
 
-
-createApp({
+// iniziamo ad usare il metodo appena importato, ritornando la mia applicazione e montandola su #app
+createApp({ //option object
     data() {
         return {
-            
+            toDoList: toDoList,
 
         }
-    }
-})
+    },
+    methods: { // object methods
+        
+    },
+    mounted() {
+        console.log(this.toDoList)
+    } //lifecycle hook, inseriamo un console.log per controllare se this.toDoList funziona
+}).mount('#app')
 
