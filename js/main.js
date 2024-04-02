45687125bf272a629f4d040f79a320c08542f989
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-02 16:18:28 +0200

    (js) add methods to create new element and (html) keyup.enter snippet

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-import {toDoList} from './data.js'
+import { toDoList } from './data.js'
 // destrutturazione del metodo createApp, mettendola nella variabile vue
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 // iniziamo ad usare il metodo appena importato, ritornando la mia applicazione e montandola su #app
 createApp({ //option object
     data() {
         return {
             toDoList: toDoList,
-
+            elementText: ''
         }
     },
     methods: { // object methods
@@ -18,9 +18,9 @@ createApp({ //option object
                 return el.id === id;
             })
             // generiamo il toggle
-            if(idTDL) {
+            if (idTDL) {
                 idTDL.done = !idTDL.done;
-            }            
+            }
         },
 
         // inseriamo un metodo per eliminare l'elemento selezionato dalla lista
@@ -28,9 +28,32 @@ createApp({ //option object
             // prendiamo l'idice dell'elemento
             const index = this.toDoList.findIndex((el) => el.id === id);
             // rimuoviamo l'elemento
-            if(index !== -1) {
+            if (index !== -1) {
                 this.toDoList.splice(index, 1);
             }
+        },
+
+        // inseriamo un metodo per aggiujere un elemento alla lista
+        addElement() {
+            const newElem = {
+                id: null,
+                text: this.elementText,
+                done: false
+            }
+             // generiamo l'id del nuovo elemento
+            let newId = 0;
+            // per ogni elemento nella lista
+            this.toDoList.forEach((el) => {
+                if (newId < el.id) {
+                    newId = el.id;
+                }
+            });
+            // il nuovo itemento ha come id l'id massimo + 1
+            newElem.id = newId + 1;
+            //ripuliamo l'input
+            this.elementText = '';
+            // pushamo il nuovo elemento nella lista
+            this.toDoList.push(newElem);
         }
     },
 
